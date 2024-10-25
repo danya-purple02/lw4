127809bef9499e4e246955e43d81a7a7e1ed7c78
danya-purple02 <iza.darkwoodenn@gmail.com>
2024-10-25 16:30:54 +0300

    additional task done

--- p4_1/p4_1/main.cpp
+++ p4_1/p4_1/main.cpp
@@ -4,84 +4,89 @@ using namespace std;
 
 void print_tree(struct Node* r, int l);
 struct Node* CreateTree(struct Node* root, struct Node* r, int data);
-//int CheckAmount(struct Node* t, int data, int r);
-struct Node* SearchElement(struct Node* t, int data);
+int CheckAmount(struct Node* t, int data, int r);
+void heroin(struct Node* r);
 
-struct Node 
+struct Node
 {
 	int data;
 	struct Node* left;
 	struct Node* right;
 };
-struct Node* root,* res;
+struct Node* root;
+int v = 0;
 
 int main()
 {
-	int D, choise = 0, start = 1, search_value = 0;
-	//int res = 0;
+	int D, choise = 0, start = 1, search_value = 0, res = 0;
 	root = NULL;
-	res = NULL;
-	while (1) 
+	while (1)
 	{
 		cout << "choose action:" << endl;
-		cout << "1 - create binary tree" << endl << "2 - search element" << endl << "3 - print tree" << endl << "4 - exit program" << endl;
+		cout << "1 - create binary tree" << endl << "2 - check amount of eq. elements" << endl << "3 - print tree" << endl << "4 - depth-first search" << endl << "5 - exit program" << endl;
 		cin >> choise;
-		switch (choise) 
+		switch (choise)
 		{
-			case 1: 
+		case 1:
+		{
+			if (root != NULL)
 			{
-				if (root != NULL) 
+				cout << "tree already exists" << endl << endl;
+				continue;
+			}
+			cout << "to stop enter -1" << endl;
+			while (start)
+			{
+				cout << "type number: ";
+				cin >> D;
+				if (D == -1)
 				{
-					cout << "tree already exists" << endl << endl;
-					continue;
+					cout << "construction is done" << endl << endl;
+					start = 0;
 				}
-				cout << "to stop enter -1" << endl;
-				while (start)
+				else
 				{
-					cout << "type number: ";
-					cin >> D;
-					if (D == -1)
-					{
-						cout << "construction is done" << endl << endl;
-						start = 0;
-					}
-					else 
-					{
-						root = CreateTree(root, root, D);
-					}
+					root = CreateTree(root, root, D);
 				}
-				continue;
 			}
-			case 2: 
+			continue;
+		}
+		case 2:
+		{
+			cout << "enter search value: ";
+			cin >> search_value;
+			res = CheckAmount(root, search_value, 0);
+			if (!res)
 			{
-				cout << "enter search value: ";
-				cin >> search_value;
-				res = SearchElement(root, search_value);
-				if (!res)
-				{
-					continue;
-				}
-				cout << "element exists" << endl << endl;
 				continue;
 			}
-			case 3: 
-			{
-				print_tree(root, 0);
-				cout << endl << endl;
-				continue;
-			}
-			case 4: 
-			{
-				return 0;
-			}
-			default: 
-			{ 
-				cout << "must type 1 - 4" << endl;
-				continue;
-			}
+			cout << "amount of equal \"" << search_value << "\": " << res << endl << endl;
+			continue;
+		}
+		case 3:
+		{
+			print_tree(root, 0);
+			cout << endl << endl;
+			continue;
+		}
+		case 4: 
+		{
+			heroin(root);
+			cout << endl << endl;
+			continue;
+		}
+		case 5:
+		{
+			return 0;
+		}
+		default:
+		{
+			cout << "must type 1 - 5" << endl;
+			continue;
+		}
 		}
 	}
-	
+
 }
 
 
@@ -116,18 +121,14 @@ struct Node* CreateTree(struct Node* root, struct Node* r, int data)
 		r->left = NULL;
 		r->right = NULL;
 		r->data = data;
+		v++;
 		if (root == NULL) return r;
 
 		if (data > root->data)	root->left = r;
 		else root->right = r;
 		return r;
 	}
-	if (data == r->data) 
-	{
-		cout << "element already exist" << endl;
-		return root;
-	}
-	if (data > r->data) 
+	if (data > r->data)
 	{
 		CreateTree(r, r->left, data);
 	}
@@ -138,34 +139,9 @@ struct Node* CreateTree(struct Node* root, struct Node* r, int data)
 	return root;
 }
 
-struct Node* SearchElement(struct Node* t, int data)
-{
-	if (t == NULL)
-	{
-		cout << "element does not exist" << endl;
-		return 0;
-	}
-
-	if (data > t->data)
-	{
-		t = t->left;
-		SearchElement(t, data);
-	}
-	else if (data < t->data)
-	{
-		t = t->right;
-		SearchElement(t, data);
-	}
-	else if (data == t->data)
-	{
-		return t;
-	}
-}
-
-/*
 int CheckAmount(struct Node* t, int data, int r)
 {
-	if (t == NULL) 
+	if (t == NULL)
 	{
 		cout << "element does not exist" << endl << endl;
 		return 0;
@@ -184,15 +160,25 @@ int CheckAmount(struct Node* t, int data, int r)
 	else if (data == t->data)
 	{
 		r++;
-		if (t->right != NULL) 
+		if (t->right != NULL)
 		{
 			t = t->right;
 			CheckAmount(t, data, r);
 		}
-		else 
+		else
 		{
 			return r;
 		}
 	}
 }
-*/
+
+void heroin(struct Node* r) 
+{
+	if (r == NULL) 
+	{
+		return;
+	}
+	cout << r->data << "  ";
+	heroin(r->left);
+	heroin(r->right);
+}
